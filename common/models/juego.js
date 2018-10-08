8dc81822e815f240261c3b52851f9c9192077b0b
Reyes <7798013@alu.murciaeduca.es>
2018-10-08 09:14:26 +0200

    this juego

--- common/models/juego.js
+++ common/models/juego.js
@@ -2,4 +2,22 @@
 
 module.exports = function(Juego) {
 
+
+
+/**
+ * devuelve el nombre de un juego
+ * @param {Function(Error, string)} callback
+ */
+
+Juego.prototype.getNombre = function(callback) {
+
+  var juego=this;
+  callback(null, juego.nombre);
+};
+
+
+
+
+
+
 };
